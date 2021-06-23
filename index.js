f98d8dd2bee294a61d5f66feacea43261d9a1fa1
Shibily-kms <kmsshibily@gmail.com>
2021-06-23 12:50:22 +0530

    contact form created

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ function mobileMenu() {
 }
 
 
+$("#submit-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:"",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
+
+        }
+    })
+})
+
